3d7810be0fc95c73dee8e7f97fbd2f1f0fd466b2
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2015-10-29 08:27:30 +0100

    Added click-button functionality

--- RosJoystick/RosJoystick.cpp
+++ RosJoystick/RosJoystick.cpp
@@ -1,44 +1,47 @@
 #include <Arduino.h>
-#include "ros.h"
-#include "std_msgs/UInt16MultiArray.h"
+#include <ros.h>
+#include "arduino_robot/Joystick.h"
 
 #define X_AXIS 6
 #define Y_AXIS 7
-#define BUTTON 13
-
+#define BUTTON 12
 #define DELAY 200
 
-unsigned int button = 0;
+int button_state = 0;
 
-std_msgs::UInt16MultiArray jdata;
-ros::Publisher pub_jdata("joystick", &jdata);
+arduino_robot::Joystick jdata;
+
+ros::Publisher pub_jdata("/arduino_robot/joystick", &jdata);
 ros::NodeHandle  nh;
 
 void setup()
 {
-  nh.initNode();
-  jdata.data_length = 2;
-  nh.advertise(pub_jdata);  
-  
-  pinMode(X_AXIS, INPUT);
-  pinMode(Y_AXIS, INPUT);
-  pinMode(BUTTON, INPUT);
-  digitalWrite(BUTTON, HIGH);
+    nh.initNode();
+    nh.advertise(pub_jdata);  
+
+    pinMode(X_AXIS, INPUT);
+    pinMode(Y_AXIS, INPUT);
+
+    // Activate pull-up resistor for button
+    pinMode(BUTTON, INPUT_PULLUP);
 }
 
-long publisher_timer;
+unsigned long publisher_timer;
 
 void loop()
 {
-  if (millis() > publisher_timer) {
+    if (millis() > publisher_timer) {
 
-    jdata.data[0] = analogRead(X_AXIS);
-    jdata.data[1] = analogRead(Y_AXIS);
-    delay(50);
+        jdata.x = analogRead(X_AXIS);
+        jdata.y = analogRead(Y_AXIS);
 
-    publisher_timer = millis() + DELAY;
-    pub_jdata.publish(&jdata);
-  }
-  
-  nh.spinOnce();
+        jdata.button = digitalRead(BUTTON) ? 0 : 1;
+
+        delay(75);
+
+        publisher_timer = millis() + DELAY;
+        pub_jdata.publish(&jdata);
+    }
+
+    nh.spinOnce();
 }
